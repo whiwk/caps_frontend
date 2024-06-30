25783e74fc6511fae9b1fe6f2c4e7c1b5c1ec5f1
whiwk <bpras747@gmail.com>
2024-06-30 20:31:54 +0700

    update on base url

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getNewToken, isTokenExpired } from '../utils/tokenService';
 
 const api = axios.create({
-  baseURL: 'http://10.30.1.221:8000/api/v1/'
+  baseURL: 'https://arigints.my.id/api/v1/'
 });
 
 api.interceptors.request.use(
